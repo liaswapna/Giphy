bd48822a8f325a920ae2a67595be7c0b0563c75b
Lia <liaswapna@gmail.com>
2018-11-07 23:18:21 -0800

    update fav button

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -7,7 +7,7 @@ var favourites = [];
 // funtion to render the buttons.
 function renderButton(){
     $("#button-list").empty();
-    for(i=0;i<topics.length;i++){
+    for(var i=0;i<topics.length;i++){
         var newBtn = $("<button>");
         newBtn.attr("data-name",topics[i]);
         newBtn.text(topics[i]);
@@ -41,6 +41,86 @@ $("#add-movie").on("click",function(event){
     }
 });
 
+// Button click event to show the favourites.
+$("#show-fav").on("click",function(event){
+    event.preventDefault();
+    $("#gif-div").empty();
+
+    for(var i=0;i<favourites.length;i++){
+        var favqueryURL = `http://api.giphy.com/v1/gifs/${favourites[i]}?api_key=A2Hw4RAXUAp9JUnpyZh9PqNapjox1Tj6`;
+        //var favqueryURL = `http://api.giphy.com/v1/gifs/search?q=${movieName}&api_key=A2Hw4RAXUAp9JUnpyZh9PqNapjox1Tj6&limit=${previousCount}`;
+    console.log("favqueryURL: "+favqueryURL);
+    $.ajax({
+        url: favqueryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var favresult = response.data;
+        console.log(favresult);
+        console.log("fav-type: "+favresult.type);
+        console.log("favstillurl: "+favresult.images.fixed_height_still.url);
+        // var favDiv = $("<div>");
+        // favDiv.text("<p>"+favourites[i]+"</p>");
+        // $("#gif-div").append(favDiv);
+        // get the value, store the value and create a div to append.
+        var stillurl = favresult.images.fixed_height_still.url;
+        var animateurl = favresult.images.fixed_height.url;
+        var rating = favresult.rating.toUpperCase();
+        var picId = favresult.id;
+        var newDiv = $("<div>");
+        newDiv.attr("id","gif-rating-div");
+        newDiv.addClass("text-center");
+
+        // create the image tag and append newDiv.
+        var newImage = $("<img>");
+        newImage.addClass("gif");
+        newImage.attr("alt","MyImage");
+        newImage.attr("status","still");
+        newImage.attr("src",stillurl);
+        newImage.attr("still-url",stillurl);
+        newImage.attr("animate-url",animateurl);
+        newDiv.append(newImage);
+
+        // create the rating <p> tag and append to newDiv.
+        var newP = $("<p>Rating: "+rating+"</p>");
+        newP.addClass("text-center");
+        newP.attr("id","rating");
+        newDiv.append(newP);
+
+        // create the <button> and <a> tag an append to newDiv.
+        var downButton = $("<button>");
+        downButton.text("Download");
+        downButton.addClass("btn btn-primary btn-sm downBtn");
+        downButton.attr("style","margin-bottom:10px;border-radius:30px;");
+        var newIcon = $("<i>");
+        newIcon.addClass("fa fa-download");  
+        downButton.prepend(newIcon);
+
+        // create anchor tag  and append with newDiv.
+        var newAnchor = $("<a>");
+        newAnchor.attr("href",stillurl);
+        newAnchor.attr("download","file.jpg");
+        newAnchor.attr("target","_blank");
+        newAnchor.attr("id","anchor-image")
+        newAnchor.append(downButton);
+        newDiv.append(newAnchor);
+
+        // create button tag and the star icon
+        var remButton = $("<button>");
+        remButton.text("Remove");
+        remButton.addClass("btn btn-info btn-sm removeFavButton");
+        remButton.attr("style","margin-left:10px; margin-bottom:10px;border-radius:30px;");
+        remButton.attr("movie-name",picId);
+        // var newSpan = $("<span>");
+        // newSpan.addClass("glyphicon glyphicon-star");
+        // newButton2.append(newSpan);
+        newDiv.append(remButton);
+        $("#gif-div").append(newDiv);
+    });
+    }
+
+});
+
 // function to display the GIF images.
 function displayGIF(){
     var movieName = $(this).attr("data-name");
@@ -65,6 +145,8 @@ function displayGIF(){
             var stillurl = result[i].images.fixed_height_still.url;
             var animateurl = result[i].images.fixed_height.url;
             var rating = result[i].rating.toUpperCase();
+            var picId = result[i].id;
+            console.log(picId);
             var newDiv = $("<div>");
             newDiv.attr("id","gif-rating-div");
             newDiv.addClass("text-center");
@@ -108,7 +190,7 @@ function displayGIF(){
             favButton.text("Favourite");
             favButton.addClass("btn btn-info btn-sm favouriteButton");
             favButton.attr("style","margin-left:10px; margin-bottom:10px;border-radius:30px;");
-            favButton.attr("movie-name","")
+            favButton.attr("movie-name",picId);
             // var newSpan = $("<span>");
             // newSpan.addClass("glyphicon glyphicon-star");
             // newButton2.append(newSpan);
@@ -134,7 +216,16 @@ function toggleGIF(){
 }
 
 function addFavourite(){
+    var favId = $(this).attr("movie-name");
+    //console.log("favourite ID: ",favId);
+    favourites.push(favId);
+}
 
+function removeFavourite(){
+    var remId = $(this).attr("movie-name");
+    var remIndex = favourites.indexOf(remId);
+    favourites.splice(remIndex,1);
+    console.log("after splice: "+favourites);
 }
 
 // gif image click event.
@@ -145,6 +236,9 @@ $(document).on("click", ".movie", displayGIF);
 
 // movie favourite button click event.
 $(document).on("click", ".favouriteButton", addFavourite);
+
+// movie favourite remove button.
+$(document).on("click", ".removeFavButton", removeFavourite);
 
 // call the renderButton function.
 renderButton();
